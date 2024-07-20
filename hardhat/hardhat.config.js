fa89368da5460b4261e5177fe7f0dd164eb89dc2
Sajesh Macbook <ss5123ic.ac.uk>
2024-07-20 18:17:47 +0100

    followed doc this time

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,18 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+};
+
+require('@nomicfoundation/hardhat-ethers');
+require('@nomicfoundation/hardhat-ignition-ethers');
+const privateKey = '0dd43f4b28dcdd0d95c11221f829dc356cde9eac52d23e02da0862a85ed5dfb9';
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    moonbase: {
+      url: 'https://rpc.api.moonbase.moonbeam.network',
+      chainId: 1287,
+      accounts: [privateKey],
+    },
+  }
+};
