47b83f99a68fda6c38e8168b0f207102f1983350
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2024-07-21 02:38:03 +0100

    add main file and more apis

--- hardhat/apiConnect.js
+++ hardhat/apiConnect.js
@@ -4,7 +4,7 @@ const MoonbaseKey = 'T4TZ4R331WUR4KPIF5TQHRR6T8TIDMI6K3'
 
 "https://api-moonbase.moonscan.io/api?module=account&action=txlist&address=0x1038F9E41836b3E8BBD3416fbdbE9836F1EA83C5&startblock=0&endblock=latest&page=1&offset=3&sort=asc&apikey=".concat(MoonbaseKey);
 
-async function getTransactionsByWalletAddress(address){
+export async function getTransactionsByWalletAddress(address){
     //console.log(`https://api-moonbase.moonscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=3&sort=asc&apikey=${MoonbaseKey}`)
     //const res = fetch(`https://api-moonbase.moonscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=3&sort=asc&apikey=${MoonbaseKey}`)
     // if (!res.ok) {
@@ -14,27 +14,27 @@ async function getTransactionsByWalletAddress(address){
     // res.then((response)=>{const jsonPromise = response.json();
     //     jsonPromise.then((data)=>{console.log(data)});
     // })
-    const res = await fetch(`https://api-moonbase.moonscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=3&sort=asc&apikey=${MoonbaseKey}`)
+    const res = await fetch(`https://api-moonbeam.moonscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=latest&page=1&offset=3&sort=asc&apikey=${MoonbaseKey}`)
     const data = await res.json();
     if(data.status!='1'){throw new Error(`HTTP Error! Status: ${res.status}`)}
     return data.result
 }
-
-async function getTokenTransferEventsByAddress(address,contractAddress){ //NOT TESTED!!!
-    const res = await fetch(`https://api-moonbase.moonscan.io/api
-   ?module=account
-   &action=tokentx
-   &contractaddress=${contractAddress}
-   &address=${address}
-   &page=1
-   &offset=100
-   &startblock=0
-   &endblock=99999999
-   &sort=asc
-   &apikey=${MoonbaseKey}`)
+export async function getTokenTransferEventsByAddress(address){
+   const res = await fetch(`https://api-moonbeam.moonscan.io/api?module=account&action=tokentx&address=${address}&page=1&offset=10&startblock=0&endblock=latest&sort=asc&apikey=${MoonbaseKey}`)
    const data = await res.json();
    if(data.status!='1'){throw new Error(`HTTP Error! Status: ${res.status}`)}
    return data.result
 }
-
-console.log(await getTransactionsByWalletAddress('0xa620f8357B449928965D34EC67249251f14d8E77'))
+export async function getTokenTransferEventsByAddressAndContractAddress(address,contractAddress){
+    const res = await fetch(`https://api-moonbeam.moonscan.io/api?module=account&action=tokentx&contractaddress=${contractAddress}&address=${address}&page=1&offset=10&startblock=0&endblock=latest&sort=asc&apikey=${MoonbaseKey}`)
+    const data = await res.json();
+    if(data.status!='1'){throw new Error(`HTTP Error! Status: ${res.status}`)}
+    return data.result
+ }
+// module.exports = {
+//     getTransactionsByWalletAddress,
+//     getTokenTransferEventsByAddress
+// }
+const wGLMRTokenContract = '0xAcc15dC74880C9944775448304B263D191c6077F'
+//console.log(await getTransactionsByWalletAddress('0x67aCcf90FB08d79e8f67B48479d7eA424D94B64e'))
+//console.log(await getTokenTransferEventsByAddressAndContractAddress(`0xd27cece5ce8b4f6fa47a160105d842f97aee227d`,'0xAcc15dC74880C9944775448304B263D191c6077F'))
